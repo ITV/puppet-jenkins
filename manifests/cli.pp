6e20ad5f1cc35b019836c2887b3e7f6bc924693c
Tom Haynes <tom.haynes@itv.com>
2020-05-22 14:37:17 +0100

    fix jenkins-cli install

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -25,8 +25,8 @@ class jenkins::cli {
   }
 
   $jar = "${jenkins::libdir}/jenkins-cli.jar"
-  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war WEB-INF/jenkins-cli.jar"
-  $move_jar = "mv WEB-INF/jenkins-cli.jar ${jar}"
+  $download_jar = 'wget http://127.0.0.1:${jenkins::port}${jenkins::prefix}/jnlpJars/jenkins-cli.jar -O /tmp/jenkins-cli.jar'
+  $move_jar = "mv /tmp/jenkins-cli.jar ${jar}"
   $remove_dir = 'rm -rf WEB-INF'
 
   # make sure we always call Exec[jenlins-cli] in case
@@ -36,7 +36,7 @@ class jenkins::cli {
     creates => $jar,
   }
   ~> exec { 'jenkins-cli' :
-    command     => "${extract_jar} && ${move_jar} && ${remove_dir}",
+    command     => "${download_jar} && ${move_jar}",
     path        => ['/bin', '/usr/bin'],
     cwd         => '/tmp',
     refreshonly => true,
